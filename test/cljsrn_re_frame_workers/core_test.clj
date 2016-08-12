c7616edac9770aa0f3c9c5f18b9232cc4aafad4b
Sean Tempesta <sean.tempesta@gmail.com>
2016-08-12 13:48:49 +0200

    new lein template

---
+++ test/cljsrn_re_frame_workers/core_test.clj
@@ -0,0 +1,7 @@
+(ns cljsrn-re-frame-workers.core-test
+  (:require [clojure.test :refer :all]
+            [cljsrn-re-frame-workers.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
